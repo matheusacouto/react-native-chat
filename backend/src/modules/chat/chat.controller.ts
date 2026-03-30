5c39f45a1b721f061102e574b705dca50e974249
Matheus Couto <matheusazcouto@gmail.com>
2026-03-30 19:38:56 -0300

    feat: add ChatController for managing chat functionalities

---
+++ backend/src/modules/chat/chat.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+import { ChatService } from './chat.service';
+
+@Controller('chat')
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+  //  Todo
+}
