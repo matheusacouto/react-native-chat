66807c460a0a84e36f31e166fa63720a6e0ee469
Matheus Couto <matheusazcouto@gmail.com>
2026-03-30 19:36:43 -0300

    refactor: rename and reorganize login DTO for Firebase authentication

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { LoginWithFireBaseDTO } from './dto/login-dto';
+import { LoginWithFireBaseDto } from './dto/login-with-firebase-dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('login')
-  loginWithFirebase(@Body() body: LoginWithFireBaseDTO) {
+  @Post('login/firebase')
+  loginWithFirebase(@Body() body: LoginWithFireBaseDto) {
     return this.authService.loginWithFirebase(body.idToken);
   }
 }
--- backend/src/modules/auth/dto/login-dto.ts
+++
@@ -1,3 +0,0 @@
-export class LoginWithFireBaseDTO {
-  idToken: string;
-}
