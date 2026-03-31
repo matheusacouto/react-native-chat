899a925f0eada761fedaf83434cdf48dd727eb6b
Matheus Couto <matheusazcouto@gmail.com>
2026-03-31 08:57:07 -0300

    feat: add DTOs for sending and starting chat messages

---
+++ backend/src/modules/chat/dto/send-message.dto.ts
@@ -0,0 +1,5 @@
+export class SendMessageDto {
+  currentUserId: number;
+  targetUserId: number;
+  text: string;
+}
---
+++ backend/src/modules/chat/dto/start-chat.dto.ts
@@ -0,0 +1,4 @@
+export class StartChatDto {
+  currentUserId: number;
+  targetUserId: number;
+}
